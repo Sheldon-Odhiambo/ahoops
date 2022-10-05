5d5c5d61041b00bf9c228fe8c9e1d78e9bc9bfd1
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-05 16:40:13 +0300

    feat(Footer.js): creates components

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import TwitterIcon from "./Twitter.jpg";
+import LinkedInIcon from "./linkedin.jpg";
+import InstagramIcon from "./instagram.jpg";
+import FacebookIcon from "./facebook.jpg";
 
 const Footer = () => {
   return (
@@ -6,7 +10,11 @@ const Footer = () => {
       <footer>
         <div className="container container-flex">
             <div className="icons">
-                
+                <TwitterIcon />
+                <LinkedInIcon />
+                <InstagramIcon />
+                <FacebookIcon />
+
             </div>
 
         </div>
