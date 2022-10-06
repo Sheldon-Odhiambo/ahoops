079c47a316dee1726ceb55dfe9b3776f6c82d873
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-06 20:37:07 +0300

    feat(Policy.js): creates Policy component

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../Css/Contact.css";
 
 const Contact = () => {
   return (
--- src/component/Policy.js
+++ src/component/Policy.js
@@ -1,9 +1,63 @@
 import React from "react";
+import "../Css/Policy.css"
 
 const Policy = () => {
   return (
     <>
-      <h1>Policy page</h1>
+      <h1>Privacy Policy</h1>
+      <div className="policy">
+        <ol>
+          <li>
+            Equal opportunity policy,Many countries mandate that you must be an
+            equal opportunity employer by law. For example, in the United States
+          </li>
+          <li>
+            Workplace health and safety,It is important to provide your
+            employees with a safe and healthy work environment, especially since
+            workplace health and safety violations can cause harm to your
+            employees,
+          </li>
+          <li>
+            Employee code of conduct policy,A clear and concise code of conduct
+            can help employees understand your r expectations in terms of
+            performance and behavior.
+          </li>
+          <li>
+            Ethics policy,A clear and concise code of conduct can help employees
+            understand your r expectations in terms of performance and behavior.{" "}
+          </li>
+          <li>
+            Travel policies,A clear and concise code of conduct can help
+            employees understand your r expectations in terms of performance and
+            behavior.
+          </li>
+          <li>
+            Remote work policy,It is important to provide your employees with a
+            safe and healthy work environment, especially since workplace health
+            and safety violations can cause harm to your employees,
+          </li>
+          <li>
+            Inclement weather policies,It is important to provide your employees
+            with a safe and healthy work environment, especially since workplace
+            health and safety violations can cause harm to your employees,
+          </li>
+          <li>
+            Workplace security policies,Observe the way your management and
+            employees deal with workplace issues, and identify which areas could
+            use improvement
+          </li>
+          <li>
+            Employee fraternization policy,Observe the way your management and
+            employees deal with workplace issues, and identify which areas could
+            use improvement
+          </li>
+          <li>
+            Substance abuse policies,Observe the way your management and
+            employees deal with workplace issues, and identify which areas could
+            use improvement
+          </li>
+        </ol>
+      </div>
     </>
   );
 };
