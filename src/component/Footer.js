914056d5dd8a7f208a79e2ea99e1b5003e731fd9
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-06 10:33:04 +0300

    fix(footer.js):debugging

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,35 +1,50 @@
 import React from "react";
-import { CssBaselinefrom } from "@mui/material";
-import Box from "@mui/material";
-import Typography from "@mui/material";
-import Container from "@mui/material";
-import Link from "@mui/material"
-import { FiInstagram} from "react-icons/fi";
+import { CssBaseline } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { FiInstagram } from "react-icons/fi";
 
-function CopyRight(){
-    
+function CopyRight() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com">
+        Your Website
+      </Link>{""}
+      {new Date().getFullYear()}{"."}
+    </Typography>
+  );
 }
-
 
 const Footer = () => {
   return (
     <>
-      <div className="container container-flex">
-        <div className="icons">
-          {/* <TwitterIcon className="icon" />
-          <LinkedInIcon className="icon" />
-          <InstagramIcon className="icon" />
-          <FacebookIcon className="icon" /> */}
-        </div>
-        <div className="line">
-          <hr />
-          <hr />
-        </div>
-        <div className="copyright">
-          <p>All rights reserved &copy;</p>
-          <p>Made with love by Mitch</p>
-        </div>
-      </div>
+    <Box sx={{
+      display:"flex",
+      flexDirection:"column",
+      minHeight: "100vh",
+    }}
+    >
+      <CssBaseline/>
+      <Container component="main" sx={{mt:8, mb:2}} maxWidth="sm"></Container>
+      <Box component="footer" 
+      sx={{
+        py:3,
+        px:2,
+        mt: "auto",
+        backgroundColor:(theme)=> theme.palette.mode==="light"? theme.palette.grey[200]: theme.palette.grey[800],
+      }}
+      >
+        <Container maxWidth="sm">
+          <FiInstagram />
+          <CopyRight />
+        </Container>
+      </Box>
+  
+    </Box>
+      
     </>
   );
 };
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import List from "../component/List";
 import Footer from "../component/Footer";
 
 const Home=()=>{
     return (
       <>
-        <List />
         <h1>Home page</h1>
         <Footer />
       </>
