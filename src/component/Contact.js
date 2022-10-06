39a27edb7e913fedc6a021dd4bcdfe2d89309383
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-06 19:56:49 +0300

    fix(Contact.js): debugging

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -9,7 +10,25 @@ const Contact = () => {
           <label htmlFor="name">Name</label>
           <input type="text" placeholder="Enter your name" />
         </div>
-
+        <div>
+          <label htmlFor="name">Email</label>
+          <input type="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Enter your password" />
+        </div>
+        <div>
+          <label htmlFor="mobile">Phone Number</label>
+          <input type="mobile" placeholder="Enter your phone number" />
+        </div>
+        <div className="btnBox">
+          <div className="btn">
+            <NavLink to="/" className="readmore">
+              Submit Details
+            </NavLink>
+          </div>
+        </div>
       </form>
     </>
   );
