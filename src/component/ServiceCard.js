5eed7d3b0c48d40cc667094f4acee5e6d2f134b1
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-07 03:54:35 +0300

    updating div class card

--- src/component/ServiceCard.js
+++ src/component/ServiceCard.js
@@ -1,23 +1,22 @@
 import React from "react";
-import ImageHome from "./Home.jpg";
+import ImageHome from "./service.jpg";
 import { NavLink } from "react-router-dom";
 
 const Combined = () => {
 	return (
 		<>
 			<div className="card">
-				<h2>Web Design</h2>
+				<h2>Best in Class to field Basketball training</h2>
 				<img src={ImageHome} alt="solution1" className="serviceimage" />
 				<p>
-					Responsive Design Regardless of your website's goals or the audience
-					that you are serving, your website will assuredly benefit from
-					incorporating a web design that works across different screen sizes
-					and on various devices.
+				Training · Shooting · Ball Handling · Offensive Moves (for Guards and Post Players) · Footwork · Rebounding · Defense · Speed and Strength Development.
 				</p>
+				<p>Group sessions that provide personalised training to improve technique and abilities.
+The training sessions at Team Sports Services (Team Basketball Academy) are tailored to different ages (ages 4 to 18) and different levels (Beginner to Intermediate B) of kids’ knowledge. The focus each training program is on fundamental skill acquisition and players progress through our unique format to integrate these skills into game knowledge incorporating strategic and tactical training.</p>
 				<div className="btnBox">
 					<div className="btn">
 						<NavLink to="/about" className="readmore">
-							Begin Learning{" "}
+							Let's Hoop{" "}
 						</NavLink>
 					</div>
 				</div>
