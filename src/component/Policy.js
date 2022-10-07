efc14bcef4e9e0cfa6ae6e8abf651cd282839b13
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-07 03:54:52 +0300

    updates on code of conduct

--- src/component/Policy.js
+++ src/component/Policy.js
@@ -1,46 +1,65 @@
 import React from "react";
-import "../Css/Policy.css"
+import "../Css/Policy.css";
 
 const Policy = () => {
-  return (
-    <>
-      <h1>Privacy Policy</h1>
-      <div className="policy">
-        <ol>
-          <li>
-           
-          </li>
-          <li>
-          
-          </li>
-          <li>
-         
-          </li>
-          <li>
- 
-          </li>
-          <li>
-          
-          </li>
-          <li>
-           
-          </li>
-          <li>
-           
-          </li>
-          <li>
-          
-          </li>
-          <li>
-           
-          </li>
-          <li>
-            
-          </li>
-        </ol>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h1>Coaching</h1>
+			<div className="policy">
+      <h3>Directors and coaches will ensure:</h3>
+
+				<ul>
+					<li>
+						Communicate regularly and extensively with players and parents about
+						all practices, tournaments and what is expected for FBA to run
+						successfully.
+					</li>
+					<li>
+						Place the emotional and physical well-being of our players ahead of
+						a personal desire to win.
+					</li>
+					<li>
+						Treat each player as an individual, remembering the large range of
+						emotional and physical development for the same age group.
+					</li>
+					<li>
+						Take reasonable measures to honor all commitments to athletes.
+					</li>
+					<li>
+						Do our best to provide a safe playing environment for our players.
+					</li>
+					<li>
+						Provide a sports environment for our teams that is free of drugs,
+						tobacco, and alcohol, and we will refrain from their use at all
+						youth sports events.
+					</li>
+					<li>
+						Do our best to organize practices that are fun and challenging for
+						all our players.
+					</li>
+					<li>
+						Be knowledgeable in the rules of basketball and will teach these
+						rules to our players.
+					</li>
+				</ul>
+        <h3>The AHOOPS expects a style of play that is both fundamentally sound and reflects a high level of effort by the student-athlete. All coaches are expected to adhere to the highest code of conduct and are teaching the following principles:</h3>
+<ul>
+  <li>Technical Instruction</li>
+  <li>Fundamentals of basketball</li>
+  <li>Skill development</li>
+  <li>Strategic and tactical development</li>
+  <li>Physical Development</li>
+  <li>Conditioning</li>
+  <li>Preparation for games and practices</li>
+  <li>Psychological development</li>
+  <li>Think positively about game</li>
+  <li>Treat student-athletes and opponents with respect</li>
+  <li>Model the type of behavior expected of student-athletes</li>
+
+</ul>
+			</div>
+		</>
+	);
 };
 
 export default Policy;
